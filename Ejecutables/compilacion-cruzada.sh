b967ad05a2e1903de07e71c9c3514c74ffa5c554
Martina Pauer <martinapauer38@gmail.com>
2025-03-30 18:32:33 -0300

    Update compilacion-cruzada.sh
    mas automatización incluida, estado inicial para compilar a IBM AIX de PowerPC que se suele usar para servidores y estaciones de trabajo para software cientifico, de negocios y demas.
    
    https://en.wikipedia.org/wiki/IBM_AIX

--- Ejecutables/compilacion-cruzada.sh
+++ Ejecutables/compilacion-cruzada.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
 # Variables sys y arch para compilar a varios sistemas y arquitecturas multiples archivos en go
-sys="darwin"
-arch="amd64"
+sys="aix"
+arch="ppc64"
+prefix=".."
+compilados="$prefix/fibonacci $prefix/ciclos $prefix/leer $prefix/escribir $prefix/logica $prefix/saludo"
+ejecutables="Ejecutables/IBM_AIX/ppc64"
 # Compilo cada uno de los archivos
-GOOS=$sys GOARCH=$arch go build fibonacci.go
-GOOS=$sys GOARCH=$arch go build ciclos.go
-GOOS=$sys GOARCH=$arch go build leer.go
-GOOS=$sys GOARCH=$arch go build escribir.go
-GOOS=$sys GOARCH=$arch go build logica.go
-GOOS=$sys GOARCH=$arch go build saludo.go
+GOOS=$sys GOARCH=$arch go build $prefix/fibonacci.go
+GOOS=$sys GOARCH=$arch go build $prefix/ciclos.go
+GOOS=$sys GOARCH=$arch go build $prefix/leer.go
+GOOS=$sys GOARCH=$arch go build $prefix/escribir.go
+GOOS=$sys GOARCH=$arch go build $prefix/logica.go
+GOOS=$sys GOARCH=$arch go build $prefix/saludo.go
+# Muevo archivos ejecutables a la carpeta de ejecutables correspondiente
+mv $compilados $ejecutables
