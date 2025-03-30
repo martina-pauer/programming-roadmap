259a008c372881243c6b9501f0830b46e8fae2eb
Martina Pauer <martinapauer38@gmail.com>
2025-03-30 21:48:48 +0000

    correccion de errores en el script para que funcione

--- Ejecutables/compilacion-cruzada.sh
+++ Ejecutables/compilacion-cruzada.sh
@@ -3,8 +3,8 @@
 sys="aix"
 arch="ppc64"
 prefix=".."
-compilados="$prefix/fibonacci $prefix/ciclos $prefix/leer $prefix/escribir $prefix/logica $prefix/saludo"
-ejecutables="Ejecutables/IBM_AIX/ppc64"
+compilados="fibonacci ciclos leer escribir logica saludo"
+ejecutables="IBM_AIX/ppc64"
 # Compilo cada uno de los archivos
 GOOS=$sys GOARCH=$arch go build $prefix/fibonacci.go
 GOOS=$sys GOARCH=$arch go build $prefix/ciclos.go
@@ -13,4 +13,4 @@ GOOS=$sys GOARCH=$arch go build $prefix/escribir.go
 GOOS=$sys GOARCH=$arch go build $prefix/logica.go
 GOOS=$sys GOARCH=$arch go build $prefix/saludo.go
 # Muevo archivos ejecutables a la carpeta de ejecutables correspondiente
-mv $compilados $ejecutables
+mv $compilados -t $ejecutables
